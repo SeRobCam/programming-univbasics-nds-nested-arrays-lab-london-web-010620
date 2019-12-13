113329099f2b41a2e529125d6662f6febde793b5
Sergio Robles <sroblesc@gmail.com>
2019-12-13 13:39:43 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -32,8 +32,8 @@ def assembled_matrix
       "Watermelon",
       "Eggplant"
     ]
-    ]
-  
+  ]
+  #1
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
