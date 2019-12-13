5912128bb2174ccb04ca5ee14f056684bf05c9c6
Sergio Robles <sroblesc@gmail.com>
2019-12-13 13:55:54 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,8 +35,8 @@ end
 def sorted_matrix
   CONVENTIONAL_PRODUCE.sort 
   ORGANIC_PRODUCE.sort
-  produce_storage_room = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
-    return produce_storage_room
+  produce_storage_room_sorted = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+    return produce_storage_room_sorted
   
 end
 
